b97caaa0f090f5812e0e3c5b3f39673770e87f6d
Mohd Sarfraz <mdsarfraz2m@gmail.com>
2025-04-27 14:46:03 +0530

    Update landing.js

--- CarbonCalc/script/landing.js
+++ CarbonCalc/script/landing.js
@@ -39,7 +39,7 @@ Give a clear, short, beginner-friendly answer about carbon footprint or the clim
     `;
 
     try {
-      const res = await fetch('https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=AIzaSyCxKwC9vNh5oueVIBGjgBvlJ2TTLisx4bo', {
+      const res = await fetch('https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=Your_API_KEY_HERE', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
